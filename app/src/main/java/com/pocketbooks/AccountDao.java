d3ed92951b46b22252247845ee22e24f5879f4c8
Joseph W. Pollard <joseph.w.pollard@gmail.com>
2025-07-25 02:43:10 -0400

    Pocket-Books-8 Edit Account Info added insert, delete, and update methods to the AccountDao

--- app/src/main/java/com/pocketbooks/AccountDao.java
+++ app/src/main/java/com/pocketbooks/AccountDao.java
@@ -2,11 +2,24 @@ package com.pocketbooks;
 
 import android.database.Cursor;
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 @Dao
 public interface AccountDao
 {
     @Query("SELECT * FROM pocketBooksAccounts")
     Cursor getAll();
+
+    @Insert
+    void createAccount(String name, int balance);
+
+    @Delete
+    void deleteAccount(Account account);
+
+    @Update
+    void updateAccount(Account account);
+
 }
